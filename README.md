656fff2393d4ba267d103ed276c33fe81212350d
Bruno Figueiredo <21010857+bmalbusca@users.noreply.github.com>
2021-02-04 22:59:48 +0000

    Add multi-insertion option

--- README.md
+++ README.md
@@ -6,28 +6,31 @@ Standard structure for Dev directories!
 ### How to run
 
 ```bash
-1. git clone https://github.com/bmalbusca/dworkspace.git
-2. bash ./workspaces.sh
+1. cd <somehere>; git clone https://github.com/bmalbusca/dworkspace.git
+2. cp dworkspaces/workspaces.sh <your-target-dir>
+3. cd <your-target-dir>; bash ./workspaces.sh
 
 ```
 
 ### The result
 ```bash
 
-Workspaces
-├── documents
-│   └── publish
-├── <external-project>
-└── internal
-    ├── lab
-    │   ├── foo
-    │   └── <language-framework>
-    ├── notes
-    └── projects
+workspaces
+├── documents           > Used to save Markdown journals
+│   ├── notes           -----> Save researches, articles and other notes
+│   └── publish         -----> Add your Markdown notes for future blog publishing
+├── <external-work-1>
+├── <external-work-2>
+├── <external-work-3>
+└── internal            > Personal code
+    ├── lab             ----> Save all your learning stuff and code tutorials
+    │   ├── foo         ----> Random code
+    │   ├── <framework-1>
+    │   ├── <framework-2>
+    │   └── <language-1>
+    └── projects        > Personal (big) projects
         ├── current
         ├── late-2019
         └── late-2020
-
-
 
 ```
--- workspaces.sh
+++ workspaces.sh
@@ -5,30 +5,34 @@
 echo " ~~ dWorkspace, the holy script for arranging code directories ~~ "
 echo "The primary concept is to create a simple and quick way to move to other computers without  wasting time"
 echo "creating your favorite development folder struture. Often translated as a STANDARD for code organization"
-echo "created by @bmalbusca (https://github.com/bmalbusca) and based on feedback from multiple developers"
-
+echo "created by @bmalbusca (https://github.com/bmalbusca/dworkspace) and based on feedback from multiple developers"
+echo "
+NOTE: use comma (,) to add multiple targets.
+"
 
 declare -i START_DATE=$(date --date="1 years ago" +'%Y')
-declare -i END_DATE=$(date +'%Y')
+declare -i END_DATE=$(date --date="2 years ago" +'%Y')
 
-# I would like to read multiple options
-read -p "External projects: " EXTERNAL_PROJECTS
-read -p "Languages/frameworks: " TECH
+IFS=',' read -r -p "External projects: " -a EXTERNAL_PROJECTS
+IFS=',' read -r -p "Languages/frameworks: " -a SUB_LAB
 
-SUB_LAB=$TECH
 SUB_DOCUMENTS=publish
 
-##################
-# The Target
-##################
-# SUB_PROJECTS={$START_DATE,$END_DATE}
-# SUB_INTERNAL={notes,projects/$SUB_PROJECTS,lab/$SUB_LAB}
+mkdir -p    workspaces/{internal/{projects,lab/foo},documents/{$SUB_DOCUMENTS,notes}}
+mkdir -p    workspaces/internal/projects/{late-{$START_DATE,$END_DATE},current}
 
-# mkdir -p workspaces/{internal/$SUB_INTERNAL,documents/$SUB_DOCUMENTS}
-###################
+for proj in "${EXTERNAL_PROJECTS[@]}"
+do
+    mkdir -p "workspaces/$proj"
+done
 
-mkdir -p    Workspaces/{internal/{notes,projects,lab/{foo,$SUB_LAB}},$EXTERNAL_PROJECTS,documents/$SUB_DOCUMENTS}
-mkdir -p    Workspaces/internal/projects/{late-{2019..2022},current}
+
+for slab in "${SUB_LAB[@]}"
+do
+    mkdir -p "workspaces/internal/lab/$slab"
+done
+
+
 
 echo "The result:"
 tree workspaces
